ea3230fdb074fb8aa084dd5c79d9e566633621cf
O.Krasovskyi <mechanosec29@gmail.com>
2024-06-10 11:27:50 +0300

    add navbody

--- lua/plugins/codding.lua
+++ lua/plugins/codding.lua
@@ -42,4 +42,18 @@ return {
       })
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "SmiteshP/nvim-navbuddy",
+        dependencies = {
+          "SmiteshP/nvim-navic",
+          "MunifTanjim/nui.nvim",
+        },
+        opts = { lsp = { auto_attach = true } },
+        keys = { { "<leader>cn", ":Navbuddy<cr>", desc = "Nvigation" } },
+      },
+    },
+  },
 }
