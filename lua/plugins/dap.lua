797aca6cb10a36e917798354fbcddc9c60562948
O.Krasovskyi <mechanosec29@gmail.com>
2024-06-12 01:36:34 +0300

    temp fix dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,61 +1,49 @@
 return {
-  -- "mfussenegger/nvim-dap",
-  -- opts = function()
-  --   local dap = require("dap")
-  --   if not dap.adapters["pwa-node"] then
-  --     require("dap").adapters["pwa-node"] = {
-  --       type = "server",
-  --       host = "localhost",
-  --       -- port = "${port}",
-  --       executable = {
-  --         command = "node",
-  --         -- 💀 Make sure to update this path to point to your installation
-  --         args = {
-  --           LazyVim.get_pkg_path("js-debug-adapter", "/js-debug/src/dapDebugServer.js"),
-  --           -- "${port}",
-  --         },
-  --       },
-  --     }
-  --   end
-  --   if not dap.adapters["node"] then
-  --     dap.adapters["node"] = function(cb, config)
-  --       if config.type == "node" then
-  --         config.type = "pwa-node"
-  --       end
-  --       local nativeAdapter = dap.adapters["pwa-node"]
-  --       if type(nativeAdapter) == "function" then
-  --         nativeAdapter(cb, config)
-  --       else
-  --         cb(nativeAdapter)
-  --       end
-  --     end
-  --   end
-  --
-  --   local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
-  --
-  --   local vscode = require("dap.ext.vscode")
-  --   vscode.type_to_filetypes["node"] = js_filetypes
-  --   vscode.type_to_filetypes["pwa-node"] = js_filetypes
-  --
-  --   for _, language in ipairs(js_filetypes) do
-  --     if not dap.configurations[language] then
-  --       dap.configurations[language] = {
-  --         {
-  --           type = "pwa-node",
-  --           request = "launch",
-  --           name = "Launch file",
-  --           program = "${file}",
-  --           cwd = "${workspaceFolder}",
-  --         },
-  --         {
-  --           type = "pwa-node",
-  --           request = "attach",
-  --           name = "Attach",
-  --           processId = require("dap.utils").pick_process,
-  --           cwd = "${workspaceFolder}",
-  --         },
-  --       }
-  --     end
-  --   end
-  -- end,
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "mxsdev/nvim-dap-vscode-js",
+    {
+      "microsoft/vscode-js-debug",
+      opt = true,
+      build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+    },
+  },
+  config = function()
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+
+    require("dap-vscode-js").setup({
+      debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+      adapters = { "pwa-node", "pwa-chrome", "node-terminal" }, -- which adapters to register in nvim-dap
+    })
+
+    local dap = require("dap")
+    for _, language in ipairs({ "typescript", "javascript" }) do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file2",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach2",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          sourceMaps = true,
+        },
+        {
+          type = "noe-terminal",
+          request = "attach",
+          name = "Attach3",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          sourceMaps = true,
+        },
+      }
+    end
+  end,
 }
