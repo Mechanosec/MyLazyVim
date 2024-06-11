4e57871f0990ee948c707b4817a0ba18e16a6d98
O.Krasovskyi <mechanosec29@gmail.com>
2024-06-11 15:07:01 +0300

    fix eslint save

--- lua/plugins/codding.lua
+++ lua/plugins/codding.lua
@@ -56,6 +56,17 @@ return {
       },
     },
     opts = {
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
       servers = {
         vtsls = {
           settings = {
