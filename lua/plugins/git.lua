45398937c6a8497bf930884776493b2eaffa827f
O.Krasovskyi <mechanosec29@gmail.com>
2024-06-20 22:28:52 +0300

    test git blame line

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,12 @@
 return {
   { "akinsho/git-conflict.nvim", version = "*", config = true },
+  -- {
+  --   "lewis6991/gitsigns.nvim",
+  --   opts = {
+  --     current_line_blame = true,
+  --     current_line_blame_opts = {
+  --       delay = 700,
+  --     },
+  --   },
+  -- },
 }
