2f4cedd0f1d2c9cdd3896ae804568ce4e1f64264
O.Krasovskyi <mechanosec29@gmail.com>
2024-06-21 21:45:25 +0300

    add diffadd diffview and copilot

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,14 @@
 return {
   { "akinsho/git-conflict.nvim", version = "*", config = true },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gdd", ":DiffviewOpen<cr>", desc = "Diffview open" },
+      { "<leader>gdc", ":DiffviewClose<cr>", desc = "Diffview close" },
+      { "<leader>gdh", ":DiffviewFileHistory %<cr>", desc = "Current file history" },
+      { "<leader>gdH", ":DiffviewFileHistory<cr>", desc = "All files history" },
+    },
+  },
   -- {
   --   "lewis6991/gitsigns.nvim",
   --   opts = {
